f50cdbfa6dc9953e17ebd77ba9387a30ceee1643
rahulgupta20nov <rahul.gupta@aurigait.com>
2016-05-25 20:53:18 +0530

    update SuperCtrl

--- js/controllers/SuperCtrl.js
+++ js/controllers/SuperCtrl.js
@@ -13,11 +13,9 @@
         vm.selectedPanelName = vm.panelName[0][0];
 
         vm.changeFocus = changeFocus;
-
+        $state.go('dashboard');
         function changeFocus(index){
           vm.selectedPanelName = vm.panelName[index][0];
-          console.log($state.current.name);
-          console.log(vm.selectedPanelName);
           var state = vm.selectedPanelName.split(" ").join("").toLowerCase();
           $state.go(state);
         }
